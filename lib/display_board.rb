4f889f1a0f107c238c9c7b9794f2e799b658c5e7
Yiliang Chen <yiliangchen0113@hotmail.com>
2020-08-23 19:02:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board = Array.new(9))
+def display_board(board = Array.new(9, ""))
 puts "#{board[0]} | #{board[1]} | #{board[2]}"
 puts "-----------"
 puts "#{board[3]} | #{board[4]} | #{board[5]}"
